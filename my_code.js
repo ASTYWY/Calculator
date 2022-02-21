c5f2beb6a675cea7976e848c227a4aab3e606221
AS-00 <bahtiyorovanasiba332@gmail.com>
2022-02-21 21:32:04 +0300

    конец

--- my_code.js
+++ my_code.js
@@ -10,5 +10,19 @@ function calc(){
     first_number = document.getElementById('first_number').value
 symbol = document.getElementById('symbol').value
 second_number = document.getElementById('second_number').value
+
+if(symbol === '+'){
+    result = Number.parseInt(first_number) + Number.parseInt(second_number);
+}
+if(symbol === '-'){
+    result = Number.parseInt(first_number) - Number.parseInt(second_number);
+}
+if(symbol === '/'){
+    result = Number.parseInt(first_number) / Number.parseInt(second_number);
+}
+if(symbol === '*'){
+    result = Number.parseInt(first_number) * Number.parseInt(second_number);
+}
+text.textContent = result;
 }
     
